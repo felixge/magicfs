c99a1b5284c2c37a72bfdb2887d5a33b16ae99cc
Felix Geisendörfer <felix@debuggable.com>
2013-01-11 22:30:45 +0400

    Implement chain Readdir support

--- chain.go
+++ chain.go
@@ -2,28 +2,91 @@ package magicfs
 
 import (
 	"net/http"
+	"os"
 )
 
-// NewChainFs returns a new ChainFs holding the given chain.
+// NewChainFs returns a new ChainFs for the given chain.
 func NewChainFs(chain... http.FileSystem) http.FileSystem {
 	return &ChainFs{chain: chain}
 }
 
+// ChainFs implements a chained file system.
 type ChainFs struct {
 	chain []http.FileSystem
 }
 
 // Open calls Open() on every fs in the chain until it can succesfully open a
-// file which it then returns. If no fs in the chain can open the given path,
-// the last error is returned.
+// file which it then returns wrapped in a ChainFile. If no fs in the chain can
+// open the given path, the last error is returned.
+//
+// BUG(felixge) Only ENOENT errors should advance the chain, other errors
+// should be returned right away. Otherwise Open() and Readdir() can get out
+// of sync.
 func (chainFs *ChainFs) Open(path string) (http.File, error) {
 	var err error
-	for _, fs := range chainFs.chain {
+	for i, fs := range chainFs.chain {
 		file, openErr := fs.Open(path)
 		if openErr == nil {
-			return file, nil
+			return &ChainFile{
+				File: file,
+				path: path,
+				others: chainFs.chain[i+1:],
+			}, nil
 		}
 		err = openErr
 	}
 	return nil, err
+}
+
+// ChainFile acts as a proxy to an embedded http.File. It intercepts all
+// Readdir() calls.
+type ChainFile struct {
+	http.File
+	others []http.FileSystem
+	path string
+}
+
+// Readdir returns the same results as seen by the parent ChainFs.
+func (file *ChainFile) Readdir(count int) ([]os.FileInfo, error) {
+	dirs, err := file.File.Readdir(count)
+	if err != nil {
+		return dirs, err
+	}
+
+	for _, fs := range file.others {
+		otherFile, err := fs.Open(file.path)
+		if err != nil {
+			continue
+		}
+
+		remaining := 0
+		if count > 0 {
+			remaining = count - len(dirs)
+		}
+
+		otherDirs, err := otherFile.Readdir(remaining)
+		if err != nil {
+			continue
+		}
+
+		dirs = append(dirs, otherDirs...)
+	}
+
+	return removeDuplicates(dirs), nil
+}
+
+func removeDuplicates(stats []os.FileInfo) []os.FileInfo {
+	names := make(map[string]bool)
+	results := make([]os.FileInfo, 0, len(stats))
+
+	for _, stat := range stats {
+		name := stat.Name()
+		if _, ok := names[name]; ok {
+			continue
+		}
+
+		names[name] = true
+		results = append(results, stat)
+	}
+	return results
 }
--- chain_test.go
+++ chain_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestChain(t *testing.T) {
+func TestChain_Open(t *testing.T) {
 	aFs := http.Dir(fixturesDir+"/a")
 	bFs := http.Dir(fixturesDir+"/b")
 	chainFs := NewChainFs(aFs, bFs)
@@ -30,6 +30,50 @@ func TestChain(t *testing.T) {
 		t.Fatalf("unexpected: %#v", err)
 	} else if pathErr.Err != syscall.ENOENT {
 		t.Fatalf("unexpected: %#v", pathErr.Err)
+	}
+}
+
+func TestChain_Readdir(t *testing.T) {
+	aFs := http.Dir(fixturesDir+"/a")
+	bFs := http.Dir(fixturesDir+"/b")
+	chainFs := NewChainFs(aFs, bFs)
+
+	dir, err := chainFs.Open("/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dir.Close()
+
+	stats, err := dir.Readdir(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var expected = map[string]int64{
+		"1.txt": 3,
+		"2.txt": 3,
+		"3.txt": 3,
+	}
+
+	for _, stat := range stats {
+		name := stat.Name()
+		// skip hidden files (my editor creates some)
+		if string(name[0]) == "." {
+			continue
+		}
+
+		size := stat.Size()
+		if expectedSize, ok := expected[name]; !ok {
+			t.Errorf("unexpected file: %s", name)
+		} else if size != expectedSize {
+			t.Errorf("expected size: %d for %s, got: %d", expectedSize, name, size)
+		} else {
+			delete(expected, name)
+		}
+	}
+
+	if len(expected) > 0 {
+		t.Errorf("missing files: %+v", expected)
 	}
 }
 
