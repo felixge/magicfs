9b91110bfa13f5f8b17e3273a824522b3936e9a2
Felix Geisendörfer <felix@debuggable.com>
2013-01-11 21:37:02 +0400

    ENOENT test

--- chain_test.go
+++ chain_test.go
@@ -3,6 +3,8 @@ package magicfs
 import (
 	"io/ioutil"
 	"net/http"
+	"os"
+	"syscall"
 	"testing"
 )
 
@@ -21,6 +23,13 @@ func TestChain(t *testing.T) {
 		t.Fatal(err)
 	} else if string(data) != "b2\n" {
 		t.Fatalf("unexpected: %s", data)
+	}
+
+	_, err := readFile(chainFs, "/does-not-exist.txt")
+	if pathErr, ok := err.(*os.PathError); !ok {
+		t.Fatalf("unexpected: %#v", err)
+	} else if pathErr.Err != syscall.ENOENT {
+		t.Fatalf("unexpected: %#v", pathErr.Err)
 	}
 }
 
